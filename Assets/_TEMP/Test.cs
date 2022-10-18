cb9eac7a06de60099a638bac47a34d9d536e764d
Mykyta Machatov <macegee@gmail.com>
2022-10-18 13:09:20 +0300

    Test

---
+++ Assets/_TEMP/Test.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    private string someTextToTest;
+
+    private void TestingTheGitCommits()
+    {
+        someTextToTest = "I commited something to Git!";
+    }
+}
